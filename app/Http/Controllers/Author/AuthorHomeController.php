2a1b9d56df4d24f712e75a6dec7fb4c303fd7a34
selamatriadydev <selamat.riady123@gmail.com>
2022-11-09 15:21:13 -0800

    dashboard author

--- app/Http/Controllers/Author/AuthorHomeController.php
+++ app/Http/Controllers/Author/AuthorHomeController.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Auth;
 class AuthorHomeController extends Controller
 {
     public function index(){
-        $total_news = Post::where('author_id', Auth::guard('author')->user()->id)->count();
+        $total_news         = Post::where('author_id', Auth::guard('author')->user()->id)->count();
         $total_news_publish = Post::where('is_publish', 1)->where('author_id', Auth::guard('author')->user()->id)->count();
-        $total_news_draft = Post::where('is_publish', 0)->where('author_id', Auth::guard('author')->user()->id)->count();
-        return view('author.home', compact('total_news','total_news_publish','total_news_draft'));
+        $total_news_draft   = Post::where('is_publish', 0)->where('author_id', Auth::guard('author')->user()->id)->count();
+        return view('author.home', compact('total_news','total_news_publish','total_news_draft')); 
     }
 }
